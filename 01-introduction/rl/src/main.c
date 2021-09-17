3d0af727fa5b410c6b4c1887af6527e0b2920256
Sergei Zimmerman <sstsimmerman@gmail.com>
2021-09-17 20:08:48 +0300

    feat: rerun clang-format; finish pp

--- 01-introduction/rl/src/main.c
+++ 01-introduction/rl/src/main.c
@@ -1,53 +1,51 @@
 #include <assert.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-int iabs(int x) {
-	return (x < 0) ? -x : x;
-}
+int iabs(int x) { return (x < 0) ? -x : x; }
 
 int eu_mod(int x, int y) {
-	int r;
-	
-	assert(y != 0);
-	
-	r = x % y;
-	
-	if (r < 0 ) {
-		r += iabs(y);
-	}
-	
-	return r;
+  int r;
+
+  assert(y != 0);
+
+  r = x % y;
+
+  if (r < 0) {
+    r += iabs(y);
+  }
+
+  return r;
 }
 
 int gcd(int x, int y) {
-	int q;
+  int q;
 
-	assert(y != 0);
-  
-	while (y != 0) {
-		q = eu_mod(x, y);
+  assert(y != 0);
 
-		x = y;
-		y = q;
-	}
-  
-	return y;
+  while (y != 0) {
+    q = eu_mod(x, y);
+
+    x = y;
+    y = q;
+  }
+
+  return y;
 }
 
 int main() {
-	int x, y, g, res;
-  
-	res = scanf("%d%d", &x, &y);
-  
-	if (res != 2 || y == 0) {
+  int x, y, g, res;
+
+  res = scanf("%d%d", &x, &y);
+
+  if (res != 2 || y == 0) {
     printf("%s\n", "Wrong input");
     abort();
   }
 
   g = gcd(x, y);
-  
-	printf("%d\n", g);
-  
-	return 0;	
+
+  printf("%d\n", g);
+
+  return 0;
 }
