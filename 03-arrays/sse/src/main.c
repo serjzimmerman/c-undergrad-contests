dbd916b7dc4c587bf4d5766d9080a055ce1bfe5a
Sergei Zimmerman <52955378+serjzimmerman@users.noreply.github.com>
2021-10-22 22:31:32 +0300

    feat(arrays): add scb, si, sse

---
+++ 03-arrays/sse/src/main.c
@@ -0,0 +1,58 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
+
+void swap_int(int *x, int *y) {
+  int temp;
+  temp = *x;
+  *x = *y;
+  *y = temp;
+}
+
+void selection_sort_iteration(int * const arr, const int len, const int last) {
+  int i, min;
+  
+  min = last;
+  
+  for (i = last; i < len; i++) {
+    if (arr[i] < arr[min]) {
+      min = i;
+    }
+  }
+
+  swap_int(&arr[last], &arr[min]);
+}
+
+void print_array(const int * const arr, const int len) {
+  int i;
+
+  for (i = 0; i < len; i++) {
+    printf("%d ", arr[i]);
+  }
+
+  printf("\n");
+}
+
+int main() {
+  int i, len, pos, *array;
+
+  if (scanf("%d", &len) != 1) {
+    abort();
+  }
+
+  array = malloc(sizeof(int) * len);
+
+  for (i = 0; i < len; i++) {
+    scanf("%d", &array[i]);
+  }
+
+  if (scanf("%d", &pos) != 1) {
+    abort();
+  }
+
+  selection_sort_iteration(array, len, pos);
+  print_array(array, len);
+
+  return 0;
+}
