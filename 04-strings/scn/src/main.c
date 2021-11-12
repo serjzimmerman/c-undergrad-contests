10974f479ab4161e0cf8ca17bea312bce3803c22
Sergei Zimmerman <52955378+serjzimmerman@users.noreply.github.com>
2021-11-12 17:48:10 +0300

    feat(string): solved scn, sr

---
+++ 04-strings/scn/src/main.c
@@ -0,0 +1,26 @@
+#include <assert.h>
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int get_string_sum(char *string) {
+  int sum = 0;
+
+  while (*string != '\0') {
+    sum += *string++; 
+  }
+
+  return sum;
+}
+
+int main() {
+  int sum = 0;
+  char c;
+
+  while ((c = getc(stdin)) != EOF) {
+    sum += c;
+  }
+
+  printf("%d", sum);
+}
---
+++ 04-strings/sr/src/main.c
@@ -0,0 +1,62 @@
+#include <assert.h>
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+void swap_chars(char *x, char *y) {
+  char temp = *x;
+  *x = *y;
+  *y = temp;
+}
+
+void revstrn(char *str, int n) {
+  int low = 0, high = n - 1;
+
+  while (low <= high) {
+    swap_chars(&str[high--], &str[low++]);
+  }
+}
+
+void readnc(FILE *stream, char *str, const int n) {
+  char c;
+  int i = 0;
+
+  while (isspace(c = getc(stream))) {
+    ;
+  }
+  while (i < n) {
+    str[i++] = c;
+    c = getc(stream); 
+  }
+  str[i] = '\0';
+}
+
+void reverse_substrings(char *str, const char *substr, const int n) {
+  char *traveller;
+  int stringlen = strlen(str);
+
+  traveller = str;
+
+  while ((traveller = strstr(traveller, substr)) != NULL && traveller - str < stringlen) {
+    revstrn(traveller, n);
+    traveller = traveller + n;
+  }
+}
+
+int main() {
+  int sublen = 0, stringlen = 0;
+  char *string, *substring;
+
+  scanf("%d", &sublen);
+  substring = calloc(1, sizeof(char) * (sublen + 1));
+  readnc(stdin, substring, sublen);
+  
+  scanf("%d", &stringlen);
+  string = calloc(1, sizeof(char) * (stringlen + 1));
+  readnc(stdin, string, stringlen);
+
+  reverse_substrings(string, substring, sublen);
+  printf("%s\n", string);
+}
