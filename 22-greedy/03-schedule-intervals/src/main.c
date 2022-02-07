7a217810c553ea7c5b6df1dc3dcd789f6a8e1044
Sergei Zimmerman <sstsimmerman@gmail.com>
2022-02-07 08:38:23 +0300

    feat(greedy): add 03-schedule-intervals

--- 22-greedy/03-schedule-intervals/src/main.c
+++ 22-greedy/03-schedule-intervals/src/main.c
@@ -9,12 +9,37 @@ struct intvl_t {
   int fin;
 };
 
-int intvl_cmp_by_fin(struct intvl_t *a, struct intvl_t *b) { return a->fin - b->fin; }
+int intvl_cmp_by_fin(struct intvl_t *a, struct intvl_t *b) {
+  return a->fin - b->fin;
+}
 
 void schedule_sort_by_fin(struct intvl_t *reqs, int nreqs) {
   qsort(reqs, nreqs, sizeof(struct intvl_t), (int (*)(const void *, const void *))intvl_cmp_by_fin);
 }
 
-int schedulemax(struct intvl_t *reqs, int nreqs) {}
+int schedulemax(struct intvl_t *reqs, int nreqs) {
+  struct intvl_t prev;
+  int n = 1, i;
 
-int main() { return 0; }
+  if (nreqs == 0) {
+    return 0;
+  }
+
+  schedule_sort_by_fin(reqs, nreqs);
+  prev = reqs[0];
+
+  for (i = 0; i < nreqs; i++) {
+    if (reqs[i].start <= prev.fin) {
+      continue;
+    } else {
+      prev = reqs[i];
+      n++;
+    }
+  }
+
+  return n;
+}
+
+int main() {
+  return 0;
+}
