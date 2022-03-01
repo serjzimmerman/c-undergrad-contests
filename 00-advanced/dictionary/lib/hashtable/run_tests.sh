e2cd44eb01116ebab15bd6c1cbf861a9e40fddc5
Sergei Zimmerman <52955378+serjzimmerman@users.noreply.github.com>
2022-03-01 04:48:50 +0300

    test(dictionary): modified build system & add run_tests.sh

--- 00-advanced/dictionary/lib/hashtable/run_tests.sh
+++ 00-advanced/dictionary/lib/hashtable/run_tests.sh
@@ -4,23 +4,20 @@ red=`tput setaf 1`
 green=`tput setaf 2`
 reset=`tput sgr0`
 
-for executable in testmemoryoptimized testspeedoptimized
+echo "Testing ${green}${executable}${reset}"
+for file in $_base/test*.dat
 do
-  echo "Testing ${green}${executable}${reset}"
-  for file in $_base/test*.dat
-  do
-    count=`echo $file | egrep -o [0-9]+`
-    echo -n "Testing ${green}${file}${reset} ... "
-    ./test/bin/${executable} -i $file > $_base/temp.dat
-    if diff -Z ${_base}/ans${count}.dat ${_base}/temp.dat; then
-      echo "${green}Passed${reset}"
-    else
-      echo "${red}Failed${reset}"
-      result=`cat ${_base}/temp.dat`
-      expected=`cat ${_base}/ans${count}.dat`
-      echo -e "Result: \t$result"
-      echo "--------------------------------"
-      echo -e "Expected: \t$expected"
-    fi
-  done
+  count=`echo $file | egrep -o [0-9]+`
+  echo -n "Testing ${green}${file}${reset} ... "
+  ./test/bin/test -i $file > $_base/temp.dat
+  if diff -Z ${_base}/ans${count}.dat ${_base}/temp.dat; then
+    echo "${green}Passed${reset}"
+  else
+    echo "${red}Failed${reset}"
+    result=`cat ${_base}/temp.dat`
+    expected=`cat ${_base}/ans${count}.dat`
+    echo -e "Result: \t$result"
+    echo "--------------------------------"
+    echo -e "Expected: \t$expected"
+  fi
 done
